443b41f75cac32baf24b28242acd649189f2cfd5
Dmitrij <lukdmitrij@yandex.ru>
2024-11-05 22:45:17 +0300

    eslint start configuration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+export default {
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+      'vue/no-unused-vars': 'error'
+    }
+  }
---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import pluginVue from 'eslint-plugin-vue'
+export default [
+
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ["src/**/*.js"],
+    ignores: ["**/*.config.js"],
+    rules: {
+         semi: "error",
+        "prefer-const": "error"
+    }
+  }
+]
